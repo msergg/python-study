0dfbbc4996686baf4c6c742d7cd7f18008bf1fe9
user <asdf@test.com>
2016-11-17 21:03:58 +0200

    phone book basic add

--- task.py
+++ task.py
@@ -5,3 +5,40 @@ CRUD
 """
 
 
+CRUD_DICT = {1: 'Create phone number', 2: 'Get phone number by name',
+             3:'Update phone number by name', 4:'Delete phone number by name'}
+
+
+phone_number_dic = {}
+
+print 'Please choose what to do: '
+
+choice = 2
+
+for i, text in CRUD_DICT.items():
+    print str(i) + " : "+ text
+
+choice = int(raw_input('Input your choice:'))
+
+
+
+
+if choice == 1:
+    print '1. Please input phone number: '
+    phone_number = raw_input()
+    print '2. Please input customer name: '
+    customer = raw_input()
+    phone_number_dic[phone_number] = customer
+
+print phone_number_dic
+
+
+
+
+
+
+
+
+
+
+
