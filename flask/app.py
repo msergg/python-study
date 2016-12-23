301c8527c2033e45d7d8851881a9f166f6df44cc
user <asdf@test.com>
2016-12-23 21:01:22 +0200

    flask2

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, g
+import redis
+
+import redis
+
+
+redis_url = "10.1.14.25"
 
 
 app = Flask(__name__)
+app.secret_key = 'its very secret key'
 
 
 # @app.route('/')
@@ -10,9 +17,23 @@ app = Flask(__name__)
 #     return render_template('index.html', name=name)
 
 
+
+
+
+@app.before_request
+def before_request():
+    g.r = redis.StrictRedis(redis_url)
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    message = session.pop('message', '')
+
+    names = g.r.keys('phonebook:*')
+
+    contacts = [(name[10:], g.r.get(name)) for name in names]
+
+    return render_template('index.html', message=message, contacts=contacts)
 
 
 @app.route('/add', methods=['GET', 'POST'])
@@ -24,8 +45,40 @@ def add():
         phone = request.form['phone']
         if name and phone:
             # add to db
+            g.r.set('phonebook:' + name, phone)
+            session['message'] = 'Contact added'
+
             return redirect('/')
     return render_template('add.html', name=name, phone=phone)
+
+
+@app.route('/alter', methods = ['POST', ])
+def alter():
+    if 'del' in request.form:
+        return redirect('/del?id=' + request.form['id'])
+
+    elif 'upd' in request.form:
+        print 'update'
+        #update
+
+    return request.form['id']
+
+
+
+@app.route('/del', methods = ['GET', 'POST'])
+def delete():
+    if request.method == 'POST':
+        if 'yes' in request.form:
+            g.r.delete('phonebook:' + request.args['name'])
+            session['message'] = 'Contact deleted'
+        return redirect('/')
+
+    return render_template('delete.html', name=request.args['id'])
+
+
+
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
